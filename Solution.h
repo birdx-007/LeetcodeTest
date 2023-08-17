1ad47a4b39af9496c7e4d137ca89d3f852218f57
BirdNoX007 <birdnox007@163.com>
2023-08-17 14:58:53 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置
    https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/

--- Solution.h
+++ Solution.h
@@ -4,18 +4,33 @@ using namespace std;
 class Solution
 {
 public:
-    int mySqrt(int x) {
-        int l = 0, r = x, res = -1;
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int l = BinarySearch(nums, target - 1) + 1;
+        int r = BinarySearch(nums, target);
+        if (l <= r) {
+            return vector<int>{l, r};
+        }
+        else {
+            return vector<int>{-1, -1};
+        }
+    }
+    /// <summary>
+    /// 寻找出现target范围的右边界
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    int BinarySearch(vector<int>& nums, int target) {
+        int l = 0, r = (int)nums.size() - 1;
         while (l <= r) {
             int mid = l + ((r - l) >> 1);
-            if ((long long)mid * mid <= x) {
-                res = mid;
+            if (nums[mid] <= target) {
                 l = mid + 1;
             }
             else {
                 r = mid - 1;
             }
         }
-        return res;
+        return l - 1;
     }
 };
--- SolutionTest.h
+++ SolutionTest.h
@@ -7,7 +7,9 @@ class SolutionTest
 public:
 	void TestSolution() {
 		//std::cout << Tools::QuickPowForPositivePower(2,10) << std::endl;
-		std::cout << solution.mySqrt(0);
+		vector<int> nums = { 1,1,2,3,4,4,4,5,6 };
+		auto result = solution.searchRange(nums, 4);
+		std::cout << result[0] << " " << result[1];
 	}
 private:
 	Solution solution;
