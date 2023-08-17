dbeaa3322df926d26ec90b1255cca04206e7a019
Bx007 <1042742166@qq.com>
2023-08-17 23:34:58 +0800

    162. 寻找峰值
    https://leetcode.cn/problems/find-peak-element/

--- Solution.h
+++ Solution.h
@@ -4,33 +4,25 @@ using namespace std;
 class Solution
 {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int l = BinarySearch(nums, target - 1) + 1;
-        int r = BinarySearch(nums, target);
-        if (l <= r) {
-            return vector<int>{l, r};
-        }
-        else {
-            return vector<int>{-1, -1};
-        }
-    }
-    /// <summary>
-    /// 寻找出现target范围的右边界
-    /// </summary>
-    /// <param name="nums"></param>
-    /// <param name="target"></param>
-    /// <returns></returns>
-    int BinarySearch(vector<int>& nums, int target) {
+    int findPeakElement(vector<int>& nums) {
         int l = 0, r = (int)nums.size() - 1;
+        int res = 0;
         while (l <= r) {
             int mid = l + ((r - l) >> 1);
-            if (nums[mid] <= target) {
+            if (GetDiff(nums, mid) > 0) {
                 l = mid + 1;
+                res = mid;
             }
             else {
                 r = mid - 1;
             }
         }
-        return l - 1;
+        return res;
+    }
+    int GetDiff(vector<int>& nums, int index) {
+        if (index == 0) {
+            return INT_MAX;
+        }
+        return nums[index] - nums[index - 1];
     }
 };
--- SolutionTest.h
+++ SolutionTest.h
@@ -7,9 +7,8 @@ class SolutionTest
 public:
 	void TestSolution() {
 		//std::cout << Tools::QuickPowForPositivePower(2,10) << std::endl;
-		vector<int> nums = { 1,1,2,3,4,4,4,5,6 };
-		auto result = solution.searchRange(nums, 4);
-		std::cout << result[0] << " " << result[1];
+		vector<int> nums = { 1,2 };
+		std::cout << solution.findPeakElement(nums);
 	}
 private:
 	Solution solution;
