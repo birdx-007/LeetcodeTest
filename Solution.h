73a838dbb57221c3ec85f10316fde94212f1d618
Bx007 <1042742166@qq.com>
2023-08-20 13:56:17 +0800

    169. 多数元素
    https://leetcode.cn/problems/majority-element/

--- Solution.h
+++ Solution.h
@@ -4,16 +4,14 @@ using namespace std;
 class Solution
 {
 public:
-    const uint32_t M1 = 0x55555555; // 01010101010101010101010101010101
-    const uint32_t M2 = 0x33333333; // 00110011001100110011001100110011
-    const uint32_t M4 = 0x0f0f0f0f; // 00001111000011110000111100001111
-    const uint32_t M8 = 0x00ff00ff; // 00000000111111110000000011111111
-
-    uint32_t reverseBits(uint32_t n) {
-        n = (n >> 1) & M1 | (n & M1) << 1;
-        n = (n >> 2) & M2 | (n & M2) << 2;
-        n = (n >> 4) & M4 | (n & M4) << 4;
-        n = (n >> 8) & M8 | (n & M8) << 8;
-        return n >> 16 | n << 16;
+    int majorityElement(vector<int>& nums) {
+        int candidate = -1, count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
     }
 };
