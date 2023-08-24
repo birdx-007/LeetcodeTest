ea1b7e98a89c43844c27b3931421dfa02cd88b9f
Bx007 <1042742166@qq.com>
2023-08-24 22:31:09 +0800

    150. 逆波兰表达式求值
    https://leetcode.cn/problems/evaluate-reverse-polish-notation/

--- Solution.h
+++ Solution.h
@@ -2,19 +2,45 @@
 #include "ListNode.h"
 #include "TreeNode.h"
 #include <vector>
+#include <stack>
 #include <string>
 #include <unordered_set>
 #include <algorithm>
 using namespace std;
 class Solution {
 public:
-    int climbStairs(int n) {
-        int p = 0, q = 0, r = 1;
-        for (int i = 0; i < n; i++) {
-            p = q;
-            q = r;
-            r = p + q;
+    int evalRPN(vector<string>& tokens) {
+        stack<string> operationSpace;
+        for (string token : tokens) {
+            if (token.length() == 1) {
+                char c = token[0];
+                if (c == '+' || c == '-' || c == '*' || c == '/') {
+                    int right = atoi(operationSpace.top().c_str());
+                    operationSpace.pop();
+                    int left = atoi(operationSpace.top().c_str());
+                    operationSpace.pop();
+                    int result;
+                    switch (c)
+                    {
+                    case '+':
+                        result = left + right;
+                        break;
+                    case '-':
+                        result = left - right;
+                        break;
+                    case '*':
+                        result = left * right;
+                        break;
+                    default:
+                        result = left / right;
+                        break;
+                    }
+                    operationSpace.push(to_string(result));
+                    continue;
+                }
+            }
+            operationSpace.push(token);
         }
-        return r;
+        return atoi(operationSpace.top().c_str());
     }
 };
