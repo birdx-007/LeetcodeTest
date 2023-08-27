ca40b73003b99f34145e1cf628ac9bc3f73bcdfc
BirdNoX007 <birdnox007@163.com>
2023-08-27 15:32:37 +0800

    191. 位1的个数
    https://leetcode.cn/problems/number-of-1-bits/

--- Solution.h
+++ Solution.h
@@ -9,40 +9,11 @@
 using namespace std;
 class Solution {
 public:
-    string simplifyPath(string path) {
-        stack<string> files;
-        int start = 0, end = 0;
-        int n = path.length();
-        string file = "";
-        while (true) {
-            while (start < n && path[start] == '/') {
-                start++;
-                end++;
-            }
-            while (end < n && path[end] != '/') {
-                end++;
-            }
-            if (end == n && start == end) {
-                break;
-            }
-            file = path.substr(start, end - start);
-            if (file == "..") {
-                if (!files.empty()) {
-                    files.pop();
-                }
-            }
-            else if (file != ".") {
-                files.push(file);
-            }
-            start = end;
-        }
-        string res = "";
-        while (!files.empty()) {
-            res = "/" + files.top() + res;
-            files.pop();
-        }
-        if (res == "") {
-            res = "/";
+    int hammingWeight(uint32_t n) {
+        int res = 0;
+        while (n) {
+            res += (n & 1);
+            n >>= 1;
         }
         return res;
     }
--- SolutionTest.h
+++ SolutionTest.h
@@ -7,7 +7,6 @@ class SolutionTest
 public:
 	void TestSolution() {
 		//std::cout << Tools::QuickPowForPositivePower(2,10) << std::endl;
-		std::cout << solution.simplifyPath("/a/./b/..");
 	}
 private:
 	Solution solution;
