96780f312f7b9807911281dd2326993481fa66a7
BirdNoX007 <birdnox007@163.com>
2023-08-28 15:44:35 +0800

    36. 有效的数独
    https://leetcode.cn/problems/valid-sudoku/

--- Solution.h
+++ Solution.h
@@ -9,17 +9,28 @@
 using namespace std;
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int tmp;
-        for (int i = 0; i < n / 2; i++) {
-            for (int j = 0; j < (n + 1) / 2; j++) {
-                tmp = matrix[i][j];
-                matrix[i][j] = matrix[n - j - 1][i];
-                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
-                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
-                matrix[j][n - i - 1] = tmp;
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int rows[9][9];
+        int columns[9][9];
+        int subboxes[3][3][9];
+
+        memset(rows, 0, sizeof(rows));
+        memset(columns, 0, sizeof(columns));
+        memset(subboxes, 0, sizeof(subboxes));
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    int index = c - '0' - 1;
+                    rows[i][index]++;
+                    columns[j][index]++;
+                    subboxes[i / 3][j / 3][index]++;
+                    if (rows[i][index] > 1 || columns[j][index] > 1 || subboxes[i / 3][j / 3][index] > 1) {
+                        return false;
+                    }
+                }
             }
         }
+        return true;
     }
 };
