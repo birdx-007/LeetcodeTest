f459a3144c463dd0cd6e6a3640ea79765f18be1f
BirdNoX007 <birdnox007@163.com>
2023-08-28 15:10:35 +0800

    48. 旋转图像
    https://leetcode.cn/problems/rotate-image/

--- Solution.h
+++ Solution.h
@@ -9,11 +9,17 @@
 using namespace std;
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        int res = 0;
-        for (int i = 0; i < (int)nums.size(); i++) {
-            res ^= nums[i];
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int tmp;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < (n + 1) / 2; j++) {
+                tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = tmp;
+            }
         }
-        return res;
     }
 };
