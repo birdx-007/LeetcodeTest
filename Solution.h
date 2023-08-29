7c7b7dc721b5e092a143cc8af6ce930f797e1de0
BirdNoX007 <birdnox007@163.com>
2023-08-29 16:05:34 +0800

    54. 螺旋矩阵
    https://leetcode.cn/problems/spiral-matrix/

--- Solution.h
+++ Solution.h
@@ -6,30 +6,72 @@
 #include <queue>
 #include <string>
 #include <unordered_set>
+#include <utility>
 #include <algorithm>
 using namespace std;
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
-        if (target <= nums[0]) {
-            return 0;
-        }
-        if (target > nums[(int)nums.size() - 1]) {
-            return (int)nums.size();
-        }
-        int l = 0, r = nums.size() - 1;
-        while (l <= r) {
-            int m = l + ((r - l) >> 1);
-            if (nums[m] < target) {
-                l = m + 1;
+    enum direction {
+        right, down, left, up
+    };
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> res;
+        int cnt = 0, m = matrix.size(), n = matrix[0].size();
+        direction d = right;
+        bool hasMoved = true;
+        int i = 0, j = 0;
+        while (cnt < m * n) {
+            if (hasMoved) {
+                res.push_back(matrix[i][j]);
+                cnt++;
             }
-            else if (nums[m] > target) {
-                r = m - 1;
-            }
-            else {
-                return m;
-            }
+            hasMoved = moveToNextPos(d, i, j, m, n);
         }
-        return l;
+        return res;
+    }
+    bool moveToNextPos(direction& d, int& i, int& j, const int& m, const int& n) {
+        switch (d)
+        {
+        case right:
+            if (j < n - i - 1) {
+                j++;
+                if (j == n - i - 1) {
+                    d = down;
+                }
+                return true;
+            }
+            d = down;
+            return false;
+        case down:
+            if (i < j - n + m) {
+                i++;
+                if (i == j - n + m) {
+                    d = left;
+                }
+                return true;
+            }
+            d = left;
+            return false;
+        case left:
+            if (j > m - i - 1) {
+                j--;
+                if (j == m - i - 1) {
+                    d = up;
+                }
+                return true;
+            }
+            d = up;
+            return false;
+        default:
+            if (i > j + 1) {
+                i--;
+                if (i == j + 1) {
+                    d = right;
+                }
+                return true;
+            }
+            d = right;
+            return false;
+        }
     }
 };
--- SolutionTest.h
+++ SolutionTest.h
@@ -7,6 +7,11 @@ class SolutionTest
 public:
 	void TestSolution() {
 		//std::cout << Tools::QuickPowForPositivePower(2,10) << std::endl;
+		vector<vector<int>> matrix = { {1,2,3},{4,5,6},{7,8,9} };
+		vector<int> res = solution.spiralOrder(matrix);
+		for (int i = 0; i < (int)res.size(); i++) {
+			std::cout << res[i] << ' ';
+		}
 	}
 private:
 	Solution solution;
