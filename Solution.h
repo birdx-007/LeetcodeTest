28ea84e7fec01c6adaa32db3c996d1de5ee2257c
BirdNoX007 <birdnox007@163.com>
2023-08-30 12:14:12 +0800

    198. 打家劫舍
    https://leetcode.cn/problems/house-robber/

--- Solution.h
+++ Solution.h
@@ -11,17 +11,13 @@
 using namespace std;
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        vector<vector<int>> f(n, vector<int>(n));
-        f[0][0] = triangle[0][0];
-        for (int i = 1; i < n; ++i) {
-            f[i][0] = f[i - 1][0] + triangle[i][0];
-            for (int j = 1; j < i; ++j) {
-                f[i][j] = min(f[i - 1][j - 1], f[i - 1][j]) + triangle[i][j];
-            }
-            f[i][i] = f[i - 1][i - 1] + triangle[i][i];
+    int rob(vector<int>& nums) {
+        int a = 0, b = nums[0], c = nums[0];
+        for (int i = 1; i < (int)nums.size(); i++) {
+            c = max(b, a + nums[i]);
+            a = b;
+            b = c;
         }
-        return *min_element(f[n - 1].begin(), f[n - 1].end());
+        return c;
     }
 };
