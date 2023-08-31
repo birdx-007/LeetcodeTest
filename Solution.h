db9a9562e667a2d1e533fa4d4dd858a107bbbfc7
BirdNoX007 <birdnox007@163.com>
2023-08-31 16:37:21 +0800

    130. 被围绕的区域
    https://leetcode.cn/problems/surrounded-regions/

--- Solution.h
+++ Solution.h
@@ -12,28 +12,47 @@
 using namespace std;
 class Solution {
 public:
-    vector<double> averageOfLevels(TreeNode* root) {
-        vector<double> res;
-        queue<TreeNode*> q;
-        int levelNum;
-        long long levelSum;
-        q.push(root);
-        while (!q.empty()) {
-            levelNum = q.size();
-            levelSum = 0;
-            for (int i = 0; i < levelNum;i++) {
-                TreeNode* p = q.front();
-                q.pop();
-                levelSum += p->val;
-                if (p->left) {
-                    q.push(p->left);
+    void solve(vector<vector<char>>& board) {
+        int m = board.size(), n = board[0].size();
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                setDirty(board, i, 0);
+            }
+            if (board[i][n - 1] == 'O') {
+                setDirty(board, i, n - 1);
+            }
+        }
+        for (int j = 1; j < n - 1; j++) {
+            if (board[0][j] == 'O') {
+                setDirty(board, 0, j);
+            }
+            if (board[m - 1][j] == 'O') {
+                setDirty(board, m - 1, j);
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
                 }
-                if (p->right) {
-                    q.push(p->right);
+                else if (board[i][j] == 'A') {
+                    board[i][j] = 'O';
                 }
             }
-            res.push_back((double)levelSum / levelNum);
         }
-        return res;
+    }
+    void setDirty(vector<vector<char>>& board,const int& i,const int& j) {
+        if (!isOnBoard(board, i, j) || board[i][j] != 'O') {
+            return;
+        }
+        board[i][j] = 'A';
+        setDirty(board, i - 1, j);
+        setDirty(board, i + 1, j);
+        setDirty(board, i, j - 1);
+        setDirty(board, i, j + 1);
+    }
+    bool isOnBoard(const vector<vector<char>>& board,const int& i,const int& j) {
+        return i >= 0 && i < (int)board.size()
+            && j >= 0 && j < (int)board[0].size();
     }
 };
