edf987574f2fa0c5c12f55a9e366b2dba0b2a23e
BirdNoX007 <birdnox007@163.com>
2023-09-01 14:42:28 +0800

    215. 数组中的第K个最大元素
    https://leetcode.cn/problems/kth-largest-element-in-an-array/

--- Solution.h
+++ Solution.h
@@ -12,10 +12,38 @@
 using namespace std;
 class Solution {
 public:
-    int rangeBitwiseAnd(int left, int right) {
-        while (left < right) {
-            right = right & (right - 1);
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        buildHeap(nums, n);
+        for (int cnt = 0; cnt < k - 1; cnt++) {
+            removeHeapTop(nums, n);
         }
-        return right;
+        return nums[0];
+    }
+    void buildHeap(vector<int>& nums, int heapSize) {
+        int lastNonLeaveIndex = heapSize / 2 - 1;
+        for (int nonLeaveIndex = lastNonLeaveIndex; nonLeaveIndex >= 0; nonLeaveIndex--) {
+            maxHeapify(nums, nonLeaveIndex, heapSize);
+        }
+    }
+    void removeHeapTop(vector<int>& nums, int& heapSize) {
+        int heapTopIndex = 0, heapBottomIndex = heapSize - 1;
+        swap(nums[heapTopIndex], nums[heapBottomIndex]);
+        heapSize--;
+        maxHeapify(nums, heapTopIndex, heapSize);
+    }
+    void maxHeapify(vector<int>& nums, int nonLeaveIndex, int heapSize) {
+        int leftIndex = nonLeaveIndex * 2 + 1, rightIndex = nonLeaveIndex * 2 + 2;
+        int largestIndex = nonLeaveIndex;
+        if (leftIndex<heapSize && nums[leftIndex]>nums[largestIndex]) {
+            largestIndex = leftIndex;
+        }
+        if (rightIndex<heapSize && nums[rightIndex]>nums[largestIndex]) {
+            largestIndex = rightIndex;
+        }
+        if (largestIndex != nonLeaveIndex) {
+            swap(nums[largestIndex], nums[nonLeaveIndex]);
+            maxHeapify(nums, largestIndex, heapSize);
+        }
     }
 };
--- SolutionTest.h
+++ SolutionTest.h
@@ -7,8 +7,6 @@ class SolutionTest
 public:
 	void TestSolution() {
 		//std::cout << Tools::QuickPowForPositivePower(2,10) << std::endl;
-		vector<vector<int>> triangle = { {2}, {3, 4},{6, 5, 7},{4, 1, 8, 3} };
-		std::cout << solution.minimumTotal(triangle);
 	}
 private:
 	Solution solution;
