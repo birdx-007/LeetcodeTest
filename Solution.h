19334e40de606964048f426b47561459789997b3
BirdNoX007 <birdnox007@163.com>
2023-09-03 14:59:22 +0800

    383. 赎金信
    https://leetcode.cn/problems/ransom-note/

--- Solution.h
+++ Solution.h
@@ -12,41 +12,20 @@
 using namespace std;
 class Solution {
 public:
-    int countNodes(TreeNode* root) {
-        if (!root) {
-            return 0;
+    bool canConstruct(string ransomNote, string magazine) {
+        if (ransomNote.size() > magazine.size()) {
+            return false;
         }
-        int depth = 0, lastLevelNum = 0;
-        TreeNode* cur = root;
-        while (cur) {
-            depth++;
-            cur = cur->left;
+        int m[26] = { 0 };
+        for (char c : magazine) {
+            m[c - 'a']++;
         }
-        int left = 1 << (depth - 1), right = (1 << depth) - 1;
-        while (left <= right) {
-            int mid = left + ((right - left) >> 1);
-            if (exist(root, depth, mid)) {
-                left = mid + 1;
+        for (char c : ransomNote) {
+            if (m[c - 'a'] == 0) {
+                return false;
             }
-            else {
-                right = mid - 1;
-            }
+            m[c - 'a']--;
         }
-        return right;
-    }
-    bool exist(TreeNode* root, int level, int k) {
-        if (level == 1) {
-            return root;
-        }
-        TreeNode* cur = root;
-        for (int i = 1 << (level - 2); i > 0; i >>= 1) {
-            if ((k & i) > 0) {
-                cur = cur->right;
-            }
-            else {
-                cur = cur->left;
-            }
-        }
-        return cur;
+        return true;
     }
 };
