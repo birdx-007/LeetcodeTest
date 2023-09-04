ad696471cc0c321e7f024b8acbc9c6ff070241d7
BirdNoX007 <birdnox007@163.com>
2023-09-04 19:01:25 +0800

    530. 二叉搜索树的最小绝对差
    https://leetcode.cn/problems/minimum-absolute-difference-in-bst/

--- Solution.h
+++ Solution.h
@@ -12,21 +12,27 @@
 using namespace std;
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        check(root, p, q);
+    int getMinimumDifference(TreeNode* root) {
+        stack<TreeNode*> stk;
+        TreeNode* cur = root;
+        int pre = -1;
+        int res = 1e5;
+        while (cur || !stk.empty()) {
+            while (cur) {
+                stk.push(cur);
+                cur = cur->left;
+            }
+            cur = stk.top();
+            stk.pop();
+            if (pre == -1) {
+                pre = cur->val;
+            }
+            else {
+                res = min(res, cur->val - pre);
+                pre = cur->val;
+            }
+            cur = cur->right;
+        }
         return res;
     }
-    bool check(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == nullptr) {
-            return false;
-        }
-        bool flson = check(root->left, p, q);
-        bool frson = check(root->right, p, q);
-        if ((flson && frson) || ((root->val == p->val || root->val == q->val) && (flson || frson))) {
-            res=root;
-        }
-        return flson || frson || root->val == p->val || root->val == q->val;
-    }
-private:
-    TreeNode* res = nullptr;
 };
