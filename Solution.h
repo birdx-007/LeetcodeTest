3f55230469845d395841d4c0f5aa091a262fc8c1
BirdNoX007 <birdnox007@163.com>
2023-09-04 19:34:05 +0800

    230. 二叉搜索树中第K小的元素
    https://leetcode.cn/problems/kth-smallest-element-in-a-bst/

--- Solution.h
+++ Solution.h
@@ -12,11 +12,9 @@
 using namespace std;
 class Solution {
 public:
-    int getMinimumDifference(TreeNode* root) {
+    int kthSmallest(TreeNode* root, int k) {
         stack<TreeNode*> stk;
         TreeNode* cur = root;
-        int pre = -1;
-        int res = 1e5;
         while (cur || !stk.empty()) {
             while (cur) {
                 stk.push(cur);
@@ -24,15 +22,12 @@ public:
             }
             cur = stk.top();
             stk.pop();
-            if (pre == -1) {
-                pre = cur->val;
-            }
-            else {
-                res = min(res, cur->val - pre);
-                pre = cur->val;
+            k--;
+            if (k == 0) {
+                return cur->val;
             }
             cur = cur->right;
         }
-        return res;
+        return -1;
     }
 };
