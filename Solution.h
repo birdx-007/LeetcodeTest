bf853c71da11902b18794a1fc724181efcfc0496
BirdNoX007 <birdnox007@163.com>
2023-09-22 19:30:38 +0800

    15. 三数之和
    https://leetcode.cn/problems/3sum/

--- Solution.h
+++ Solution.h
@@ -12,22 +12,42 @@
 using namespace std;
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int n = numbers.size();
-        int index1 = 0, index2 = n - 1;
-        while (index1 < index2) {
-            int sum = numbers[index1] + numbers[index2];
-            if (sum == target) {
-                vector<int> res = { index1 + 1,index2 + 1 };
-                return res;
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        int n = nums.size();
+        if (n < 3) {
+            return res;
+        }
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                break;
             }
-            else if (sum < target) {
-                index1++;
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
             }
-            else {
-                index2--;
+            int l = i + 1, r = n - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    res.push_back({ nums[i],nums[l],nums[r] });
+                    while (l < r && nums[l] == nums[l + 1]) {
+                        l++;
+                    }
+                    while (l < r && nums[r] == nums[r - 1]) {
+                        r--;
+                    }
+                    l++;
+                    r--;
+                }
+                else if (sum > 0) {
+                    r--;
+                }
+                else {
+                    l++;
+                }
             }
         }
-        return vector<int>(2, 0);
+        return res;
     }
 };
