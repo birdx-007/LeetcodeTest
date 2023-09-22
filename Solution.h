b8ca7d903b6e7f9c07b2eff9233aa625672bad35
BirdNoX007 <birdnox007@163.com>
2023-09-22 19:39:41 +0800

    1. 两数之和
    https://leetcode.cn/problems/two-sum/

--- Solution.h
+++ Solution.h
@@ -12,42 +12,17 @@
 using namespace std;
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> indexMap;
         int n = nums.size();
-        if (n < 3) {
-            return res;
-        }
-        sort(nums.begin(), nums.end());
         for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) {
-                break;
+            if (indexMap.find(target - nums[i]) == indexMap.end()) {
+                indexMap[nums[i]] = i;
             }
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            int l = i + 1, r = n - 1;
-            while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-                if (sum == 0) {
-                    res.push_back({ nums[i],nums[l],nums[r] });
-                    while (l < r && nums[l] == nums[l + 1]) {
-                        l++;
-                    }
-                    while (l < r && nums[r] == nums[r - 1]) {
-                        r--;
-                    }
-                    l++;
-                    r--;
-                }
-                else if (sum > 0) {
-                    r--;
-                }
-                else {
-                    l++;
-                }
+            else {
+                return { indexMap[target - nums[i]],i };
             }
         }
-        return res;
+        return { -1,-1 };
     }
 };
